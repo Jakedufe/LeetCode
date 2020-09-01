5d3c60a9fb07ad7790bff5d1d6299072b8b954a9
Jake Liu <dufejake@163.com>
2020-09-01 16:54:01 +0800

    9月新进度。

---
+++ src/leetcode/editor/cn/P剑指_offer_39ShuZuZhongChuXianCiShuChaoGuoYiBanDeShuZiLcof.java
@@ -0,0 +1,63 @@
+//数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。 
+//
+// 
+//
+// 你可以假设数组是非空的，并且给定的数组总是存在多数元素。 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: [1, 2, 3, 2, 2, 2, 5, 4, 2]
+//输出: 2 
+//
+// 
+//
+// 限制： 
+//
+// 1 <= 数组长度 <= 50000 
+//
+// 
+//
+// 注意：本题与主站 169 题相同：https://leetcode-cn.com/problems/majority-element/ 
+//
+// 
+// Related Topics 位运算 分治算法 
+// 👍 53 👎 0
+
+
+package leetcode.editor.cn;
+import org.junit.Test;
+import java.util.*;
+//Java：数组中出现次数超过一半的数字
+public class P剑指_offer_39ShuZuZhongChuXianCiShuChaoGuoYiBanDeShuZiLcof{
+    
+    @Test
+    public void testResult() {
+        Solution solution = new P剑指_offer_39ShuZuZhongChuXianCiShuChaoGuoYiBanDeShuZiLcof().new Solution();      // TO TEST
+        solution.majorityElement(new int[]{1,2,3,2,2,2,5,4,2});
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int majorityElement(int[] nums) {
+        if (nums.length == 0) return 0;
+        Map<Integer,Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hashMap.containsKey(nums[i])){
+                Integer times = hashMap.get(nums[i]);
+                hashMap.put(nums[i],times+1);
+            }else {
+                hashMap.put(nums[i],1);
+            }
+        }
+        for (Integer integer : hashMap.keySet()) {
+            if (hashMap.get(integer) > nums.length/2){
+                return integer;
+            }
+        }
+        return 0;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/leetcode/editor/cn/P剑指_offer_40ZuiXiaoDeKgeShuLcof.java
@@ -0,0 +1,62 @@
+//输入整数数组 arr ，找出其中最小的 k 个数。例如，输入4、5、1、6、2、7、3、8这8个数字，则最小的4个数字是1、2、3、4。
+// 示例 1： 
+//
+// 输入：arr = [3,2,1], k = 2
+//输出：[1,2] 或者 [2,1]
+// 
+//
+// 示例 2： 
+//
+// 输入：arr = [0,1,2,1], k = 1
+//输出：[0] 
+//
+// 
+//
+// 限制： 
+//
+// 
+// 0 <= k <= arr.length <= 10000 
+// 0 <= arr[i] <= 10000 
+// 
+// Related Topics 堆 分治算法 
+// 👍 121 👎 0
+
+
+package leetcode.editor.cn;
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.*;
+//Java：最小的k个数
+public class P剑指_offer_40ZuiXiaoDeKgeShuLcof{
+    
+    @Test
+    public void testResult() {
+        Solution solution = new P剑指_offer_40ZuiXiaoDeKgeShuLcof().new Solution();        // TO TEST
+        int[] arr = new int[]{3,2,1};
+        int[] re = solution.getLeastNumbers(arr, 2);
+        Assert.assertArrayEquals(re,new int[]{1,2});
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] getLeastNumbers(int[] arr, int k) {
+        if (k == 0) return new int[0];
+        return divided(arr,k);
+    }
+    private int[] divided(int[] arr,int k){
+        while (arr.length > k){
+            int[] a1 = Arrays.copyOf(arr,arr.length/2);
+            int[] a2 = Arrays.copyOfRange(arr,arr.length/2,arr.length);
+            int[] r1 = divided(a1, k);
+            int[] r2 = divided(a2, k);
+            int[] r3 = new int[r1.length+r2.length];
+            System.arraycopy(r1, 0, r3, 0, r1.length);
+            System.arraycopy(r2, 0, r3, r1.length, r2.length);
+            Arrays.sort(r3);
+            return Arrays.copyOfRange(r3,0,k);
+        }
+        return arr;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
