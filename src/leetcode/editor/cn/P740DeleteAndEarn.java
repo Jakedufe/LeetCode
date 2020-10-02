77609409821c3bf62a89cd27766be26290c6a16f
Jake Liu <dufejake@163.com>
2020-10-02 23:39:56 +0800

    10月2日，最大上升子序列，最大子数组和打家劫舍。

--- src/leetcode/editor/cn/P740DeleteAndEarn.java
+++ src/leetcode/editor/cn/P740DeleteAndEarn.java
@@ -40,10 +40,6 @@ package leetcode.editor.cn;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
 //Java：删除与获得点数
 //2020-10-02 21:32:04
 public class P740DeleteAndEarn {
@@ -56,43 +52,74 @@ public class P740DeleteAndEarn {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+        /**
+         * hash数组
+         *
+         * @param nums
+         * @return
+         */
         public int deleteAndEarn(int[] nums) {
             int n = nums.length;
-            Map<Integer, Integer> hashMap = new HashMap<>();
+            if (n == 0) return 0;
+            int maxN = 0;
             for (int i = 0; i < n; i++) {
-                if (hashMap.containsKey(nums[i])) {
-                    Integer times = hashMap.get(nums[i]);
-                    hashMap.put(nums[i], times + 1);
-                } else {
-                    hashMap.put(nums[i], 1);
-                }
+                maxN = Math.max(maxN, nums[i]);
             }
-            int size = hashMap.size();
-            Integer[] arr = new Integer[size];
-            int index = 0;
-            for (Integer integer : hashMap.keySet()) {
-                arr[index++] = integer;
+            int size = maxN + 1;
+            int[] hash = new int[size];
+            for (int i = 0; i < n; i++) {
+                hash[nums[i]]++;
             }
-            Arrays.sort(arr, (o1, o2) -> o2 - o1);
-            if (size == 0) return 0;
             int[] dp = new int[size];
-            dp[0] = arr[0] * hashMap.get(arr[0]);
-            if (size == 1) return dp[0];
-            if (arr[0] - 1 != arr[1]) {
-                dp[1] = dp[0] + arr[1] * hashMap.get(arr[1]);
-            } else {
-                dp[1] = Math.max(dp[0], arr[1] * hashMap.get(arr[1]));
-            }
-            if (size == 2) return dp[1];
+            dp[1] = hash[1];
             for (int i = 2; i < size; i++) {
-                if (arr[i] != arr[i - 1] - 1) {
-                    dp[i] = arr[i] * hashMap.get(arr[i]) + Math.max(dp[i - 1], dp[i - 2]);
-                } else {
-                    dp[i] = Math.max(arr[i] * hashMap.get(arr[i]) + dp[i - 2], dp[i - 1]);
-                }
+                dp[i] = Math.max(dp[i - 1], dp[i - 2] + hash[i] * i);
             }
             return dp[size - 1];
         }
+
+//        /**
+//         * 自己写的又臭又长的代码
+//         * @param nums
+//         * @return
+//         */
+//        public int deleteAndEarn(int[] nums) {
+//            int n = nums.length;
+//            Map<Integer, Integer> hashMap = new HashMap<>();
+//            for (int i = 0; i < n; i++) {
+//                if (hashMap.containsKey(nums[i])) {
+//                    Integer times = hashMap.get(nums[i]);
+//                    hashMap.put(nums[i], times + 1);
+//                } else {
+//                    hashMap.put(nums[i], 1);
+//                }
+//            }
+//            int size = hashMap.size();
+//            Integer[] arr = new Integer[size];
+//            int index = 0;
+//            for (Integer integer : hashMap.keySet()) {
+//                arr[index++] = integer;
+//            }
+//            Arrays.sort(arr, (o1, o2) -> o2 - o1);
+//            if (size == 0) return 0;
+//            int[] dp = new int[size];
+//            dp[0] = arr[0] * hashMap.get(arr[0]);
+//            if (size == 1) return dp[0];
+//            if (arr[0] - 1 != arr[1]) {
+//                dp[1] = dp[0] + arr[1] * hashMap.get(arr[1]);
+//            } else {
+//                dp[1] = Math.max(dp[0], arr[1] * hashMap.get(arr[1]));
+//            }
+//            if (size == 2) return dp[1];
+//            for (int i = 2; i < size; i++) {
+//                if (arr[i] != arr[i - 1] - 1) {
+//                    dp[i] = arr[i] * hashMap.get(arr[i]) + Math.max(dp[i - 1], dp[i - 2]);
+//                } else {
+//                    dp[i] = Math.max(arr[i] * hashMap.get(arr[i]) + dp[i - 2], dp[i - 1]);
+//                }
+//            }
+//            return dp[size - 1];
+//        }
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
